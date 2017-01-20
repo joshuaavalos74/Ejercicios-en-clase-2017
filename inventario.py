87060a783621d52e001d2e898c5fe8a3421dd770
joshuaavalos74 <joshuaavalos74@gmail.com>
2017-01-19 20:39:12 -0600

    dfsdf

---
+++ inventario.py
@@ -0,0 +1,11 @@
+tienda = {}
+ingrese_cont="SI"
+while ingrese_cont == "SI":                                                                    
+	producto=(input("Ingrese el producto que desea: "))
+	print("Ingrese la cantidad de ", producto, ": ")
+	cant=int(input())
+	tienda[producto]=cant
+	ingrese_cont=(input("Desea ingresar otro producto?")
+		
+
+ 
